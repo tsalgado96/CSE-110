830a92853520fe2434b6d3a470136c4dfcd40258
TJ Salgado <tsalgado96@gmail.com>
2020-08-26 12:04:19 +0200

    Initialized files for Lab 4.

---
+++ Lab4/src/Lab4.java
@@ -0,0 +1,23 @@
+/*-------------------------------------------------------------
+// AUTHOR: Teodoro Salgado
+// FILENAME: Lab4.java
+// SPECIFICATION: your own description of the program.
+// FOR: CSE 110 - Lab #4
+// TIME SPENT:
+//-----------------------------------------------------------*/
+import java.util.Scanner;
+
+public class Lab4 {
+	public static void main(String[] args) {
+
+	} // End main()
+
+	// Print the menu.
+	private static void displayMenu() {
+		System.out.println("Please choose one option from the following menu:");
+		System.out.println("1) Calculate the sum of integers from 1 to m");
+		System.out.println("2) Calculate the factorial of a given number");
+		System.out.println("3) Display the leftmost digit of a given number");
+		System.out.println("4) Quit");
+	} // End displayMenu()
+} // End Lab4 class
