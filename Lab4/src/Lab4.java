8cb8cd48a7fd87513a926f195b26072f9b669182
TJ Salgado <tsalgado96@gmail.com>
2020-08-26 21:32:52 +0200

    Lab 4 is working.

--- Lab4/src/Lab4.java
+++ Lab4/src/Lab4.java
@@ -9,6 +9,68 @@ import java.util.Scanner;
 
 public class Lab4 {
 	public static void main(String[] args) {
+		// Declares all necessary variables
+		int userOption, userNumber, result;
+
+		Scanner input = new Scanner(System.in);
+
+		do {
+			// Display the menu
+			displayMenu();
+
+			// Ask the user for one option
+			userOption = input.nextInt();
+
+			switch (userOption) {
+
+				// Calculates and prints the sum of integers between 1 and the user inputted number
+				case 1:
+					System.out.println("Enter a number: ");
+					userNumber = input.nextInt();
+					// Initializes the result to 0
+					result = 0;
+					// Loops from the user inputted number down to 1 and adds to itself to find the sum of all integers
+					for (int i = userNumber; i > 0; i--){
+						result += i;
+					}
+					System.out.printf("The sum of 1 to %d is %d\n\n", userNumber, result);
+					break;
+
+				// Calculates the factorial of the user inputted number
+				case 2:
+					System.out.println("Enter a number: ");
+					userNumber = input.nextInt();
+					// Initializes result to 1 so that zero is not multiplied into the calculation
+					result = 1;
+					// Loops from the user inputted number down to 1 and multiplies to itself to find the factorial
+					for (int i = userNumber; i > 0; i--){
+						result *= i;
+					}
+					System.out.printf("The factorial of %d is %d\n\n", userNumber, result);
+					break;
+
+				// Finds the leftmost integer of the user inputted number
+				case 3:
+					System.out.println("Enter a number: ");
+					userNumber = input.nextInt();
+					// Stores userNumber into result so that original userNumber is not changed during calculation
+					result = userNumber;
+					// Keeps dividing the integer by 10 until it is in the range of [1, 9]
+					// The result is the leftmost digit
+					while (result > 9){
+						result /= 10;
+					}
+					System.out.printf("The leftmost digit of %d is %d\n\n",userNumber, result);
+					break;
+
+				// Prints Bye if the user inputs 4 to quit
+				case 4:
+					System.out.println("Bye");
+					break;
+			}
+
+		} while (userOption != 4); // End do while loop
+
 
 	} // End main()
 
