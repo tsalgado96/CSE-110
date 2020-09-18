7e338b99904321339f1ec71b50517af6060adae0
TJ Salgado <tsalgado96@gmail.com>
2020-09-18 09:48:31 +0200

    Finished assignment 3

--- Assignment3/src/Assignment3.java
+++ Assignment3/src/Assignment3.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 
 public class Assignment3 {
 	public static void main(String[] args) {
-		// Declare and instantiate a Scanner
+		/* DECLARE AND INSTANTIATE A SCANNER */
 		Scanner input = new Scanner(System.in);
 
-		// Declare and initialize variables
-		int currentShares, purchasePrice, marketPrice, availableFunds;
-		double totalBuyCost;
+		/* DECLARE AND INITIALIZE VARIABLES */
+		double currentShares, purchasePrice, marketPrice, availableFunds,
+				numberOfSharesToBuy, perShareBuyValue, totalBuyValue,
+				numberOfSharesToSell, perShareSellValue, totalSellValue,
+				transactionFee = 10;
 
-		// Prompt for and collect inputs
+		/* PROMPT FOR AND COLLECT INPUTS */
 		System.out.print("Current Shares  : ");
 		currentShares = input.nextInt();
 		System.out.print("Purchase Price  : ");
@@ -22,11 +24,45 @@ public class Assignment3 {
 		marketPrice = input.nextInt();
 		System.out.print("Available Funds : ");
 		availableFunds = input.nextInt();
+		System.out.println();
 
-		// Compute required values
-		totalBuyCost = 10 + marketPrice *
+		/* COMPUTE REQUIRED VALUES */
+		// Buy values
+		perShareBuyValue = purchasePrice - marketPrice;
+		numberOfSharesToBuy = Math.floor((availableFunds - transactionFee) / marketPrice);
+		totalBuyValue = perShareBuyValue * numberOfSharesToBuy;
 
-		// Display required outputs
+		// Sell values
+		perShareSellValue = marketPrice - purchasePrice;
+		totalSellValue = perShareSellValue * currentShares;
+		numberOfSharesToSell = Math.floor(totalSellValue / perShareSellValue);
 
-	}
-}
+
+		/* DISPLAY REQUIRED OUTPUTS */
+		// Sell or hold shares if purchase price is less than market price
+		if (purchasePrice < marketPrice){
+			if (totalSellValue > 10){
+				System.out.printf("Sell %d shares\n", (int)numberOfSharesToSell);
+			}
+			else {
+				System.out.println("Hold shares");
+			}
+		}
+
+		// Buy or hold shares if purchase price is greater than market price
+		else if (purchasePrice > marketPrice) {
+			if (totalBuyValue > 10) {
+				System.out.printf("Buy %d shares\n", (int)numberOfSharesToBuy);
+			}
+			else {
+				System.out.println("Hold shares");
+			}
+		}
+
+		// Hold shares if purchase price and market price are equal
+		else {
+			System.out.println("Hold shares");
+		}
+
+	} // End of main
+} // End of class
