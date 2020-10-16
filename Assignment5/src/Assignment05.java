7626347ec118f04135622a4f0bf039719b9c0ead
TJ Salgado <tsalgado96@gmail.com>
2020-10-16 14:36:21 +0200

    Initial files for Assignment 5

---
+++ Assignment5/src/Assignment05.java
@@ -0,0 +1,72 @@
+// CSE 110     : CSE 110 / Online
+// Assignment  : Assignment 5
+// Author      : Teodoro Salgado 1220358243
+// Description : 
+
+public class Assignment05 {
+
+    public static void main(String[] args) {
+        // Write any code here that you may wish to test your methods defined below.
+
+    }
+
+    // 1) Write (define) a static method named displayGreeting,
+    //  that takes no arguments and returns no value.
+    //  When this function is called, it should print the text "Hello, and welcome!".
+
+
+    //2) Write (define) a static method named displayText,
+    //  that takes a single String argument and returns no value.
+    //  When this function is called, it should print the value of the argument that was passed to it.
+
+
+    //3) Write (define) a static method named printTotal,
+    //  that takes three int arguments.
+    //  When this function is called, it should print the sum of the three arguments passed to it.
+    //  This function should return no value.
+
+
+    //4) Write (define) a static method named getTotal,
+    //  that takes three int arguments.
+    //  When this function is called, it should return the sum
+    //  of the three arguments passed to it as an int.
+
+
+    //5) Write (define) a static method named getAverage,
+    //  that takes three int arguments.
+    //  When this function is called, it should return the average
+    //  of the three arguments passed to it as a double.
+
+
+    //6) Write (define) a static method named averageLength,
+    //  that takes three String arguments.
+    //  When this function is called, it should return the average length (number of characters)
+    //  of the String arguments passed to it as a double.
+
+
+    //7) Write (define) a static method named lengthOfShortest,
+    //  that takes two String arguments. When this function is called,
+    //  it should return the length (number of characters)
+    //  of the shortest String argument passed to it as an int.
+
+
+    //8) Write (define) a static method named stringOfStars,
+    //  that takes one String argument.
+    //  When this function is called, it should return a String of asterisks (*)
+    //  that is the same length as the string argument passed to it.
+
+
+    //9) Write (define) a static method named maxStringOfStars,
+    //  that takes two String arguments.
+    //  When this function is called, it should return a String of asterisks (*)
+    //  that is the same length as the longest string argument passed to it.
+
+
+    //10) Write (define) a static method named midStringOfStars,
+    //   that takes three String arguments.
+    //   When this function is called, it should return a String of asterisks (*)
+    //   that is the same length as the string argument with the length
+    //   that would be in the middle if the lengths of the arguments were arranged in ascending order.
+
+
+}
