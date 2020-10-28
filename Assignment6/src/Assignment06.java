9097d1991f3126390e8a3030ffb6242dc9409fae
TJ Salgado <tsalgado96@gmail.com>
2020-10-28 22:46:13 +0100

    Added assignment 6 files

---
+++ Assignment6/src/Assignment06.java
@@ -0,0 +1,73 @@
+// CSE 110     : <Class #> / <meeting days and times>
+// Assignment  : <assignment #>
+// Author      : <name> & <studentID>
+// Description : <of the file contents>
+
+public class Assignment06 {
+
+    public static void main(String[] args) {
+        // Write any code here that you may wish to test your methods defined below.
+
+    }
+
+    // 1) Write a public static method named printArray,
+    //   that takes two arguments. The first argument is an Array of int
+    //   and the second argument is a String. The method should print out
+    //   a list of the values in the array, each separated by the value of the second argument.
+
+
+    // 2) Write a public static method named getFirst,
+    //    that takes an Array of int as an argument and returns
+    //    the value of the first element of the array.
+
+
+    // 3) Write a public static method named getLast,
+    //    that takes an Array of int as an argument and returns
+    //    the value of the last element of the array.
+
+
+    // 4) Write a public static method named getAllButFirst,
+    //    that takes an Array of int as an argument and creates and returns
+    //    a new array with all of the values in the argument array except the first value.
+
+
+    // 5) Write a public static method named getIndexOfMin,
+    //    that takes an Array of int as an argument and returns
+    //    the index of the least value in the array.
+
+
+    // 6) Write a public static method named getIndexOfMax,
+    //    that takes an Array of int as an argument and returns
+    //    the index of the largest value in the array.
+
+
+    // 7) Write a public static method named swapByIndex,
+    //    that takes three arguments. The first argument is an Array of int,
+    //    and the second and third arguments are int indexes.
+    //    This method will swap the values at the two given index arguments
+    //    in the array, and return a reference to the array.
+
+
+    //8) Write a public static method named removeAtIndex,
+    //  that takes two arguments. The first argument is an Array of int,
+    //  and the second argument is an int index. This method create and return
+    //  a new array with all of the values in the argument array
+    //  except the value at the argument index.
+
+
+    //9) Write a public static method named insertAtIndex,
+    //   that takes three arguments. The first argument is an Array of int,
+    //   the second argument is an int index, and the third argument is an int value.
+    //   This method create and return a new array with all of the values
+    //   in the argument array and including the third argument value
+    //   inserted at the index specified by the second argument value.
+
+
+    //10) Write a public static method named isSorted,
+    //    that takes an Array of int as an argument.
+    //    This method should return the boolean value true
+    //    if all the element values in the array are in ascending order;
+    //    otherwise the method should return the boolean value false.
+
+
+}
