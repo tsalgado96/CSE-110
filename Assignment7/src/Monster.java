09d9cb8e40116daf2b429d8f7542ec506dbe3778
TJ Salgado <tsalgado96@gmail.com>
2020-10-31 01:36:55 +0100

    Completed main logic for assignment 7

--- Assignment7/src/Monster.java
+++ Assignment7/src/Monster.java
@@ -1,2 +1,38 @@
+// CSE 110     : CSE 110 / Online
+// Assignment  : Assignment 7
+// Author      : Teodoro Salgado 1220358243
+// Description :
+
 public class Monster {
+    private String name;
+    private int healthScore;
+    private Weapon weapon;
+
+    public Monster(){
+
+    }
+
+    public Monster(String name, int healthScore, Weapon weapon){
+        this.name = name;
+        this.healthScore = healthScore;
+        this.weapon = weapon;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHealthScore(){
+        return this.healthScore;
+    }
+
+    public String getWeaponName(){
+        return this.weapon.getName();
+    }
+
+    public int attack(Monster monster){
+        int damage = (int)(Math.random()*this.weapon.getMaxDamage());
+        monster.healthScore = monster.getHealthScore() - damage;
+        return damage;
+    }
 }
--- Assignment7/src/Weapon.java
+++ Assignment7/src/Weapon.java
@@ -1,2 +1,35 @@
+// CSE 110     : CSE 110 / Online
+// Assignment  : Assignment 7
+// Author      : Teodoro Salgado 1220358243
+// Description :
+
 public class Weapon {
+    private String name;
+    private int maxDamage;
+
+    public Weapon(){
+        this.name = "Pointy Stick";
+        this.maxDamage = 1;
+    }
+
+    public Weapon(String name, int maxDamage){
+        this.name = name;
+        this.maxDamage = maxDamage;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getMaxDamage(){
+        return this.maxDamage;
+    }
+
+    public void setMaxDamage(int maxDamage){
+        this.maxDamage = maxDamage;
+    }
 }
