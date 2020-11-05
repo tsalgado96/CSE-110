ce2787f90138f73e67015288ebf12b97a2ff69c2
TJ Salgado <tsalgado96@gmail.com>
2020-11-05 10:20:01 +0100

    Assignment 8 started

--- Assignment8/src/Player.java
+++ Assignment8/src/Player.java
@@ -4,5 +4,41 @@
 // Description : <of the file contents>
 
 public class Player {
+    private String name;
+    private double attackStat;
+    private double blockStat;
 
+    public Player(){
+
+    }
+
+    public Player(String name, double attackStat, double blockStat){
+        this.name = name;
+        this.attackStat = attackStat;
+        this.blockStat = blockStat;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getAttackStat(){
+        return this.attackStat;
+    }
+
+    public void setAttackStat(double attackStat){
+        this.attackStat = attackStat;
+    }
+
+    public double getBlockStat(){
+        return this.blockStat;
+    }
+
+    public void setBlockStat(double blockStat){
+        this.blockStat = blockStat;
+    }
+
+    public void printPlayerInfo(){
+
+    }
 }
--- Assignment8/src/Roster.java
+++ Assignment8/src/Roster.java
@@ -2,7 +2,16 @@
 // Assignment  : <assignment #>
 // Author      : <name> & <studentID>
 // Description : <of the file contents>
+import java.util.ArrayList;
 
 public class Roster {
+    private ArrayList players;
 
+    public Roster(){
+        players = new ArrayList();
+    }
+
+    public Roster(String str){
+        players = new ArrayList();
+    }
 }
