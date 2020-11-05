ff419efa4367e3a79c0ce88e66b2b1d03d24619c
TJ Salgado <tsalgado96@gmail.com>
2020-11-05 10:40:11 +0100

    Assignment 7 ready for submission

--- Assignment7/src/Monster.java
+++ Assignment7/src/Monster.java
@@ -1,9 +1,10 @@
 // CSE 110     : CSE 110 / Online
 // Assignment  : Assignment 7
 // Author      : Teodoro Salgado 1220358243
-// Description :
+// Description : Creates a Monster object with a name, a health score, and a weapon
 
 public class Monster {
+    // Declares instance variables
     private String name;
     private int healthScore;
     private Weapon weapon;
@@ -12,26 +13,34 @@ public class Monster {
 
     }
 
+    // Overloaded constructor initializes the objects name, healthScore and weapon
     public Monster(String name, int healthScore, Weapon weapon){
         this.name = name;
         this.healthScore = healthScore;
         this.weapon = weapon;
     }
 
+    // Returns a copy of the value currently stored in the objects name variable
     public String getName(){
         return this.name;
     }
 
+    // Returns a copy of the value currently stored in the objects healthScore variable
     public int getHealthScore(){
         return this.healthScore;
     }
 
+    // Returns a copy of the value currently stored in the objects weapon variable
+    // Calls the getName() method from the Weapon class
     public String getWeaponName(){
         return this.weapon.getName();
     }
 
+    // Picks a random int between 0 and the attacking monsters maxDamage
+    // Subtracts that damage from the attacked monster and sets a new healthScore
+    // Returns how much damage was inflicted
     public int attack(Monster monster){
-        int damage = (int)(Math.random()*this.weapon.getMaxDamage());
+        int damage = (int)(Math.random() * this.weapon.getMaxDamage());
         monster.healthScore = monster.getHealthScore() - damage;
         return damage;
     }
--- Assignment7/src/Weapon.java
+++ Assignment7/src/Weapon.java
@@ -1,34 +1,42 @@
 // CSE 110     : CSE 110 / Online
 // Assignment  : Assignment 7
 // Author      : Teodoro Salgado 1220358243
-// Description :
+// Description : Creates a Weapon object with a name and a max damage, to be used by the Monster class
 
 public class Weapon {
+    // Declares instance variables
     private String name;
     private int maxDamage;
 
+    // Default constructor initializes the objects name variable to "Pointy Stick" and maxDamage to 1
     public Weapon(){
         this.name = "Pointy Stick";
         this.maxDamage = 1;
     }
 
+    // Overloaded constructor initializes the objects name and maxDamage variables with the variables
+    // passed in as parameters
     public Weapon(String name, int maxDamage){
         this.name = name;
         this.maxDamage = maxDamage;
     }
 
+    // Returns a copy of the value currently stored in the objects name variable
     public String getName(){
         return this.name;
     }
 
+    // Assigns the value passed in as an argument to the objects name variable
     public void setName(String name){
         this.name = name;
     }
 
+    // Returns a copy of the value currently stored in the objects maxDamage variable
     public int getMaxDamage(){
         return this.maxDamage;
     }
 
+    // Assigns the value passed in as an argument to the objects maxDamage variable
     public void setMaxDamage(int maxDamage){
         this.maxDamage = maxDamage;
     }
