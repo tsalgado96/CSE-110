5d3627cb8d9d9afd7d133352d8873cff0a12db4f
TJ Salgado <tsalgado96@gmail.com>
2020-12-02 07:17:50 +0100

    Assignment 8 ready for submission

--- Assignment8/src/Player.java
+++ Assignment8/src/Player.java
@@ -12,34 +12,40 @@ public class Player {
 
     }
 
+    // Overloaded constructor initializes the objects name, attackStat, and blockStat
     public Player(String name, double attackStat, double blockStat){
         this.name = name;
         this.attackStat = attackStat;
         this.blockStat = blockStat;
     }
 
+    // Returns the value stored in the objects name variable
     public String getName(){
         return this.name;
     }
 
+    // Returns the value currently stored in the objects attackStat variable
     public double getAttackStat(){
         return this.attackStat;
     }
 
+    // Stores the value passed in as the objects attackStat
     public void setAttackStat(double attackStat){
         this.attackStat = attackStat;
     }
 
+    // Returns the value currently stored in the objects blockStat variable
     public double getBlockStat(){
         return this.blockStat;
     }
 
+    // Stores the value passed in as the objects blockStat
     public void setBlockStat(double blockStat){
         this.blockStat = blockStat;
     }
 
+    // Prints the objects name, attackStat, and blockStat variables
     public void printPlayerInfo(){
-        //System.out.println(this.name + " (attack = " + this.attackStat + ", block = " + this.blockStat + ")");
         System.out.printf("%s (attack = %.2f, block = %.2f)\n", this.name, this.attackStat, this.blockStat);
     }
 }
--- Assignment8/src/Roster.java
+++ Assignment8/src/Roster.java
@@ -13,16 +13,20 @@ public class Roster {
         players = new ArrayList<>();
     }
 
+    // Constructor initializes objects from a text file
     public Roster(String fileName){
         players = new ArrayList<>();
         try {
             Scanner input = new Scanner(new File(fileName));
 
+            // Loop will cycle through all the "words" in the text file
             while (input.hasNext()){
+                // Concatenates the first and last names
                 String name = input.next();
                 name += " " + input.next();
                 double attackStat = input.nextDouble();
                 double blockStat = input.nextDouble();
+                // Creates a new Player object and stores it in the players ArrayList
                 players.add(new Player(name, attackStat, blockStat));
             }
         }
@@ -32,15 +36,19 @@ public class Roster {
 
     }
 
+    // Instantiates a new Player object and adds it to the players ArrayList
     public void addPlayer(String name, double attackStat, double blockStat){
         players.add(new Player(name, attackStat, blockStat));
     }
 
+    // Returns how many Player objects are in the ArrayList players
     public int countPlayers(){
         return players.size();
     }
 
     public Player getPlayerByName(String name){
+        // Loops through the player ArrayList and returns true if there is a Player object that has the arguments name
+        // Otherwise will return false
         for (Player player : players){
             if (name.equals(player.getName())){
                 return player;
@@ -50,17 +58,22 @@ public class Roster {
     }
 
     public void printTopAttackers(){
+        // Initializes the top attacker as the first item in the players ArrayList
         Player topAttacker1 = players.get(0);
         Player topAttacker2 = players.get(0);
+        // Iterates through the players ArrayList and sets the stop attacker as topAttacker1
         for (Player player : players){
             if (player.getAttackStat() > topAttacker1.getAttackStat()){
                 topAttacker1 = player;
             }
         }
 
+        // If topAttacker1 is the first item in the players ArrayList then topAttacker2 will be
+        // initialized with the second item
         if (topAttacker1 == players.get(0)){
             topAttacker2 = players.get(1);
         }
+        // Iterates through the players ArrayList and sets the stop attacker as topAttacker2
         for (Player player : players){
             if (player != topAttacker1){
                 if (player.getAttackStat() > topAttacker2.getAttackStat()){
@@ -73,17 +86,22 @@ public class Roster {
     }
 
     public void printTopBlockers(){
+        // Initializes the top blocker as the first item in the players ArrayList
         Player topBlocker1 = players.get(0);
         Player topBlocker2 = players.get(0);
+        // Iterates through the players ArrayList and sets the stop blocker as topBlocker1
         for (Player player : players){
             if (player.getBlockStat() > topBlocker1.getBlockStat()){
                 topBlocker1 = player;
             }
         }
 
+        // If topBlocker1 is the first item in the players ArrayList then topBlocker2 will be
+        // initialized with the second item
         if (topBlocker1 == players.get(0)){
             topBlocker2 = players.get(1);
         }
+        // Iterates through the players ArrayList and sets the stop attacker as topBlocker2
         for (Player player : players){
             if (player != topBlocker1){
                 if (player.getBlockStat() > topBlocker2.getBlockStat()){
@@ -95,6 +113,7 @@ public class Roster {
         topBlocker2.printPlayerInfo();
     }
 
+    // Iterates through the players ArrayList and prints each one
     public void printAllPlayers(){
         for (Player player : players){
             player.printPlayerInfo();
